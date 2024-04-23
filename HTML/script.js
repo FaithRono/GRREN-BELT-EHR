ee7d7f802aa86cf659dec1d4f0d636ff72953b3f
Owen Gitau <owengitau02@gmail.com>
2024-04-23 23:05:34 +0300

    supabase Intergration

--- HTML/script.js
+++ HTML/script.js
@@ -1,3 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
 /*const signInBtnlink = document.querySelector('.signInBtnlink');
 const signUpBtnlink = document.querySelector('.signInBtnlink');
 const wrapper = document.querySelector('.wrapper');
@@ -19,3 +20,6 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
+const { createClient } = _supabase
+ const _supabase = createClient('https://wvksmwyiajqlisnmejit.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2a3Ntd3lpYWpxbGlzbm1laml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1NjEwNzMsImV4cCI6MjAyOTEzNzA3M30.dtyej_wD3t_OT6Uwsyn1fEO43XhBPQK6xbI4CcA2wGs')
+console.log(_supabase)
